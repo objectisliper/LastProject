8e69c0ebeba439adedf20ee3b28ba993af9bf380
objectisliper <40005347+objectisliper@users.noreply.github.com>
2018-06-07 14:14:25 +0300

    Add files via upload

---
+++ ExampleForSale/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $("a.scrollto").click(function () {
+        var elementClick = '#'+$(this).attr("href").split("#")[1]
+        var destination = $(elementClick).offset().top;
+        jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 800);
+        return false;
+    });
+});
