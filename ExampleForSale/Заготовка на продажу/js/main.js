8d64eabb92c8a717abdf643e34daf11b5a352795
objectisliper <40005347+objectisliper@users.noreply.github.com>
2018-06-07 14:33:55 +0300

    Message

--- ExampleForSale/Заготовка на продажу/js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $("a.scrollto").click(function () {
-        var elementClick = '#'+$(this).attr("href").split("#")[1]
-        var destination = $(elementClick).offset().top;
-        jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 800);
-        return false;
-    });
-});
